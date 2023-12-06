45d121c73b6204bd965c2d0dff78656eebd2324e
BICHHANG\hang <21522041@gm.uit.edu.vn>
2023-12-06 13:14:29 +0700

    handle Booking Status + Modify Booking

--- controllers/tour.js
+++ controllers/tour.js
@@ -25,6 +25,54 @@ router.get("/place", async (req, res) => {
   }
 });
 
+router.get("/id", async (req, res) => {
+  const id  = req.query.id;
+  
+  try {
+    const tour = await Tour.findOne({_id :  id });
+    if (!tour) {
+      return res.status(404).json({ error: "Tour not found" });
+    }
+    res.json(tour);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+router.get("/destination", async (req, res) => {
+  const { destination} = req.query;
+  try {
+    const tour = await Tour.find({destination});
+    if (!tour) {
+      return res.status(404).json({ error: "Tour not found" });
+    }
+
+    res.json(tour);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+
+router.get("/destination/departures", async (req, res) => {
+  const { destination} = req.query;
+  try {
+    const tour = await Tour.find({destination});
+    if (!tour) {
+      return res.status(404).json({ error: "Tour not found" });
+    }
+
+    const tourDepartureSet = new Set (tour.map(tour => tour.departure))
+    const tourDeparture = Array.from(tourDepartureSet)
+    res.json(tourDeparture);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
 router.get("/vietnam-tours", async (req, res) => {
   try {
     const tour = await Tour.find({isVNTour: true});
@@ -112,5 +160,98 @@ router.get("/international-tours/destinations", async (req, res) => {
     res.status(500).json({ error: "Server error" });
   }
 });
+
+router.get("/vietnam-tours/north-destinations", async (req, res) => {
+  try {
+    const tour = await Tour.find({isVNTour: true, area: "North"});
+    if (!tour) {
+      return res.status(404).json({ error: "Tour not found" });
+    }
+    const tourDestinationSet = new Set (tour.map(tour => tour.destination))
+    const tourDestination= Array.from(tourDestinationSet)
+    res.json(tourDestination);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+router.get("/vietnam-tours/center-destinations", async (req, res) => {
+  try {
+    const tour = await Tour.find({isVNTour: true, area: "Center"});
+    if (!tour) {
+      return res.status(404).json({ error: "Tour not found" });
+    }
+    const tourDestinationSet = new Set (tour.map(tour => tour.destination))
+    const tourDestination= Array.from(tourDestinationSet)
+    res.json(tourDestination);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+router.get("/vietnam-tours/south-destinations", async (req, res) => {
+  try {
+    const tour = await Tour.find({isVNTour: true, area: "South"});
+    if (!tour) {
+      return res.status(404).json({ error: "Tour not found" });
+    }
+    const tourDestinationSet = new Set (tour.map(tour => tour.destination))
+    const tourDestination= Array.from(tourDestinationSet)
+    res.json(tourDestination);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+
+router.get("/international-tours/asia-destinations", async (req, res) => {
+  try {
+    const tour = await Tour.find({isVNTour: false, area: "Asia"});
+    if (!tour) {
+      return res.status(404).json({ error: "Tour not found" });
+    }
+    const tourDestinationSet = new Set (tour.map(tour => tour.destination))
+    const tourDestination = Array.from(tourDestinationSet)
+    res.json(tourDestination);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+router.get("/international-tours/europe-destinations", async (req, res) => {
+  try {
+    const tour = await Tour.find({isVNTour: false, area: "Europe"});
+    if (!tour) {
+      return res.status(404).json({ error: "Tour not found" });
+    }
+    const tourDestinationSet = new Set (tour.map(tour => tour.destination))
+    const tourDestination = Array.from(tourDestinationSet)
+    res.json(tourDestination);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+router.get("/international-tours/australia-destinations", async (req, res) => {
+  try {
+    const tour = await Tour.find({isVNTour: false, area: "Australia"});
+    if (!tour) {
+      return res.status(404).json({ error: "Tour not found" });
+    }
+    const tourDestinationSet = new Set (tour.map(tour => tour.destination))
+    const tourDestination = Array.from(tourDestinationSet)
+    res.json(tourDestination);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+
 
 module.exports = router;
