0e757c055b09102892e2e78ce1331e213be332d2
BICHHANG\hang <21522041@gm.uit.edu.vn>
2024-01-14 15:50:01 +0700

    fixxx

--- index.js
+++ index.js
@@ -1,61 +1,3 @@
-// const express = require("express");
-// const app = express();
-// const cors = require("cors");
-// const mongoose = require("mongoose");
-// const authRouter = require("./controllers/auth.js");
-// const tourRouter = require("./controllers/tour.js");
-// const imageRouter = require("./controllers/image.js");
-// const ratingRouter = require("./controllers/rating.js");
-// const bookingRouter = require("./controllers/booking.js");
-// const adultRouter = require("./controllers/adult.js");
-// const childrenRouter = require("./controllers/children.js");
-// const bankingAccountRouter = require("./controllers/bankingAccount.js");
-// var bodyParser = require("body-parser");
-// const { Authentication } = require("./middlewares/authentication.js");
-// const { Authorization } = require("./middlewares/authorization.js");
-// const Booking = require("./models/booking.js");
-// app.use(express.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-// app.use(cors());
-// app.use("/auth", authRouter);
-// app.use("/tour", tourRouter);
-// app.use("/image", imageRouter);
-// app.use("/rating", ratingRouter);
-// app.use("/booking", bookingRouter);
-// app.use("/adult", adultRouter);
-// app.use("/children", childrenRouter);
-// app.use("/banking-account", bankingAccountRouter);
-
-// app.get("/admin", Authentication, Authorization("admin"), async (req, res) => {
-//   res.send("admin");
-// });
-
-// app.get("/user", Authentication, Authorization("user"), async (req, res) => {
-//   res.send("user");
-// });
-
-// require("dotenv").config();
-
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
-// app.listen(3001, async () => {
-//   console.log("Server is running on port 3001");
-//   await mongoose.connect(
-//     "mongodb+srv://21522041:12032003@cluster0.cxvcnmf.mongodb.net/MyAdventure?retryWrites=true&w=majority",
-//     {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     }
-//   );
-// });
-
-// // module.exports.supabase = supabase;
-
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -72,14 +14,10 @@ var bodyParser = require("body-parser");
 const { Authentication } = require("./middlewares/authentication.js");
 const { Authorization } = require("./middlewares/authorization.js");
 const Booking = require("./models/booking.js");
-
-require("dotenv").config();
-
-app.use(cors());
-
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(cors());
 app.use("/auth", authRouter);
 app.use("/tour", tourRouter);
 app.use("/image", imageRouter);
@@ -96,6 +34,8 @@ app.get("/admin", Authentication, Authorization("admin"), async (req, res) => {
 app.get("/user", Authentication, Authorization("user"), async (req, res) => {
   res.send("user");
 });
+
+require("dotenv").config();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -114,4 +54,7 @@ app.listen(3001, async () => {
   );
 });
 
-export default app;
+// module.exports.supabase = supabase;
+
+module.exports.app = app;
+
